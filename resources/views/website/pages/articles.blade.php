aeec204eeaca78d26a1fc3f10a8016e9c2555252
Gafar <qafarzade2014@gmail.com>
2022-04-07 12:31:21 +0400

    last changesa

--- resources/views/website/pages/articles.blade.php
+++ resources/views/website/pages/articles.blade.php
@@ -9,8 +9,8 @@
             <div class="row my-2">
                 @foreach($posts as $post)
                     <div class="col-12 col-md-4 col-lg-3 my-2">
-                        <div class="card shadow-sm" style="height: 460px">
-                            <img src="{{asset(Voyager::image($post->getAttribute('image')))}}" height="304" class="card-img-top" alt="{{$post->getTranslatedAttribute('title')}}">
+                        <div class="card shadow-sm" style="height: 466px">
+                            <img src="{{asset(Voyager::image($post->getAttribute('image')))}}" class="card-img-top" alt="{{$post->getTranslatedAttribute('title')}}">
                             <div class="card-body">
                                 <a href="{{route('article', $post)}}">
                                     <h6 class="card-title">{{$post->getTranslatedAttribute('title')}}</h6>
