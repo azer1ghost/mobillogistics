423566709735a51fed1d37edd601d12b52e00a87
Veliyev <aliveliyev016@gmail.com>
2025-03-13 11:25:11 +0400

    google tag manager

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -22,6 +22,15 @@
             background-color: white;
         }
     </style>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5LX5ZDTB');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 <body>
     @include('website.components.header')
@@ -34,5 +43,10 @@
 
     @yield('scripts')
     @stack('scripts')
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LX5ZDTB"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
 </body>
 </html>
